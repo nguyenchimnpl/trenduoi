7496fe932f22cbd5d9de8154ba6e2292f6d22ba3
nguyenchimnpl <90503949+nguyenchimnpl@users.noreply.github.com>
2021-11-05 23:17:01 +0700

    abc

---
+++ data/slide2.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{F26D6119-9DA3-4279-BA67-0ED9EDB612BA}'];loadHandler&&loadHandler(1, '<div id="spr0_b20b4620"><div id="spr1_b20b4620" class="kern slide"><img id="img0_b20b4620" src="data/img1.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_b20b4620" class="kern slide"><div id="vd0_b20b4620"></div></div></div>');})();
---
+++ data/slide3.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{F26D6119-9DA3-4279-BA67-0ED9EDB612BA}'];loadHandler&&loadHandler(2, '<div id="spr0_b20b4b31"><div id="spr1_b20b4b31" class="kern slide"><img id="img0_b20b4b31" src="data/img1.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_b20b4b31" class="kern slide"><div id="vd0_b20b4b31"></div></div></div>');})();
